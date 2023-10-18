b8a5933d2eb0d388178395fae860136d2ad91b7f
Jurij Leshchuk <ddevdev609@gmail.com>
2023-10-18 08:27:16 +0300

    miss migration, add relation property to response

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -49,6 +49,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     private ?string $username = null;
 
     #[ORM\OneToMany(mappedBy: 'owner', targetEntity: DragonTreasure::class)]
+    #[Groups(['user:read', 'user:write'])]
     private Collection $dragonTreasures;
 
     public function __construct()
